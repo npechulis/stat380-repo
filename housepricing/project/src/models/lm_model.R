ed1349e39e321b0cc7dcaa64d1381c2e31a76536
npechulis <njp5739@psu.edu>
2024-02-04 17:20:47 -0500

    commit

--- housepricing/project/src/models/lm_model.R
+++ housepricing/project/src/models/lm_model.R
@@ -12,20 +12,24 @@ submission <- fread('./housepricing/project/volume/data/raw/example_sub.csv')
 
 # initialize y vector, remove NA values from test set, and combine
 y_train = train$SalePrice
+y_test = submission$SalePrice
 
 test$SalePrice <- as.numeric(test$SalePrice)
-test$SalePrice = 10
+test$SalePrice = 0
 
 master <- rbind(train,test)
 
+
+
 #test
-dummies <- dummyVars(SalePrice ~ ., data = master)
-train <- predict(dummies, newdata = train)
-test <- predict(dummies, newdata = test)
-
 train <- data.table(train)
-train$SalePrice <- y_train
 test <- data.table(test)
+train$SalePrice <- y_train
 
-lm_model <- lm(SalePrice~ ., data=train)
+lm_model <- lm(SalePrice~ LotArea + TotRmsAbvGrd + GrLivArea + BedroomAbvGr, data=train)
 summary(lm_model)
+
+# save submission
+test$SalePrice <- predict(lm_model, newdata=test)
+submission$SalePrice <- test$SalePrice
+fwrite(submission, './housepricing/project/volume/data/processed/submission.csv')
