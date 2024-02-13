85d36207c298b551c20931bca4fbcc2bdbadbd9c
npechulis <njp5739@psu.edu>
2024-02-13 14:14:58 -0500

    initalize

---
+++ housepricing - Copy/README.md
@@ -0,0 +1,1 @@
+This was my fourth submission (I got really bad scores on my first few attempts since my submission file wasn't in the right order)
---
+++ housepricing - Copy/Run_project.R
@@ -0,0 +1,3 @@
+source("./housepricing/project/required/requirements.R")
+source("./housepricing/project/src/features/Data_Cleaning.R")
+source("./housepricing/project/src/models/final_lm_model.R")
